c16d4dd8ce4e060942cea64e84c366c5791db620
Coto <programmer.coto.1988@gmail.com>
2022-08-14 03:09:19 -0400

    merge branch 'TGDS1.65' into 'master'

---
+++ arm9/source/WoopsiTemplate.cpp
@@ -0,0 +1,543 @@
+// Includes
+#include "typedefsTGDS.h"
+#include "WoopsiTemplate.h"
+#include "woopsiheaders.h"
+#include "bitmapwrapper.h"
+#include "bitmap.h"
+#include "graphics.h"
+#include "rect.h"
+#include "gadgetstyle.h"
+#include "fonts/newtopaz.h"
+#include "woopsistring.h"
+#include "colourpicker.h"
+#include "filerequester.h"
+#include "soundTGDS.h"
+#include "main.h"
+#include "posixHandleTGDS.h"
+#include "keypadTGDS.h"
+#include "gui_console_connector.h"
+#include "TGDSLogoLZSSCompressed.h"
+#include "VideoGL.h"
+#include "videoTGDS.h"
+#include "math.h"
+#include "imagepcx.h"
+#include "dswnifi_lib.h"
+#include "tamalib.h"
+#include "hal.h"
+#include "hw.h"
+#include "rom.h"
+#include "state.h"
+#include "timerTGDS.h"
+#include "cpu.h"
+#include "program.h"
+#include "mem_edit.h"
+#include "debugNocash.h"
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void enableWaitForVblankC(){
+	WoopsiTemplateProc->enableWaitForVblank();
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void disableWaitForVblankC(){
+	WoopsiTemplateProc->disableWaitForVblank();
+}
+
+__attribute__((section(".dtcm")))
+WoopsiTemplate * WoopsiTemplateProc = NULL;
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void WoopsiTemplate::startup(int argc, char **argv)   {
+	
+	//Init Main Engine (A) as 3D
+	bool isTGDSCustomConsole = true;	//set default console or custom console: custom console
+	GUI_init(isTGDSCustomConsole);
+	GUI_clear();
+	
+	//xmalloc init removes args, so save them
+	int i = 0;
+	for(i = 0; i < argc; i++){
+		argvs[i] = argv[i];
+	}
+
+	bool isCustomTGDSMalloc = true;
+	setTGDSMemoryAllocator(getProjectSpecificMemoryAllocatorSetup(TGDS_ARM7_MALLOCSTART, TGDS_ARM7_MALLOCSIZE, isCustomTGDSMalloc, TGDSDLDI_ARM7_ADDRESS));
+	sint32 fwlanguage = (sint32)getLanguage();
+	
+	//argv destroyed here because of xmalloc init, thus restore them
+	for(i = 0; i < argc; i++){
+		argv[i] = argvs[i];
+	}
+
+	switch_dswnifi_mode(dswifi_idlemode);
+	asm("mcr	p15, 0, r0, c7, c10, 4");
+	flush_icache_all();
+	flush_dcache_all();
+	
+	int ret=FS_init();
+	if (ret == 0)
+	{
+		
+	}
+	else{
+		
+	}
+	
+	//tama setup
+	//render TGDSLogo from a LZSS compressed file
+	RenderTGDSLogoMainEngine((uint8*)&TGDSLogoLZSSCompressed[0], TGDSLogoLZSSCompressed_size);
+	//apply scaling to main engine
+	REG_BG3PA = stretch_x;
+	REG_BG3PB = 0; REG_BG3PC = 0;
+	REG_BG3PD = regBG3PD;
+	REG_BG3X = regBG3X;
+	REG_BG3Y = regBG3Y;
+	
+	disableWaitForVblankC();
+    
+	//restore last state now.
+	reEnableVblank = false;
+	Rect rect;
+
+	/** SuperBitmap preparation **/
+	// Create bitmap for superbitmap
+	Bitmap* superBitmapBitmap = new Bitmap(164, 191);
+
+	// Get a graphics object from the bitmap so that we can modify it
+	Graphics* gfx = superBitmapBitmap->newGraphics();
+
+	// Clean up
+	delete gfx;
+
+	// Create screens
+	uiScreen = new AmigaScreen(TGDSPROJECTNAME, Gadget::GADGET_DECORATION, AmigaScreen::AMIGA_SCREEN_SHOW_DEPTH);
+	woopsiApplication->addGadget(uiScreen);
+	uiScreen->setPermeable(true);
+
+	// Add child windows
+	AmigaWindow* controlWindow = new AmigaWindow(0, 13, 256, 33, "Controls", Gadget::GADGET_DECORATION, AmigaWindow::AMIGA_WINDOW_SHOW_DEPTH);
+	uiScreen->addGadget(controlWindow);
+
+	// Controls
+	controlWindow->getClientRect(rect);
+
+	_Index = new Button(rect.x, rect.y, 41, 16, "<");	
+	_Index->setRefcon(1);
+	controlWindow->addGadget(_Index);
+	_Index->addGadgetEventHandler(this);
+	
+	_lastFile = new Button(rect.x + 41, rect.y, 41, 16, "v");
+	_lastFile->setRefcon(2);
+	controlWindow->addGadget(_lastFile);
+	_lastFile->addGadgetEventHandler(this);
+	
+	_nextFile = new Button(rect.x + 41 + 41, rect.y, 41, 16, ">");
+	_nextFile->setRefcon(3);
+	controlWindow->addGadget(_nextFile);
+	_nextFile->addGadgetEventHandler(this);
+	
+	_play = new Button(rect.x + 41 + 41 + 41, rect.y, 80, 16, "Save & Exit");
+	_play->setRefcon(4);
+	controlWindow->addGadget(_play);
+	_play->addGadgetEventHandler(this);
+	
+	_MultiLineTextBoxLogger = NULL;	//destroyable TextBox
+	
+	//Tama process init --
+	
+	char rom_path[256] = ROM_PATH;
+	char sprites_path[256] = {0};
+	bool_t gen_header = 0;
+	bool_t extract_sprites = 0;
+	bool_t modify_sprites = 0;
+
+	//startTimerCounter(tUnitsMilliseconds, 1000); //tUnitsMilliseconds equals 1 millisecond/unit. A single unit (1) is the default value for normal timer count-up scenarios. 
+	tamalib_register_hal(&hal);
+
+	/*
+	for (;;) {
+		int index;
+		int c;
+
+		c = getopt_long(argc, argv, short_options, long_options, &index);
+
+		if (-1 == c)
+			break;
+
+		switch (c) {
+			case 0:	// getopt_long() flag 
+				break;
+
+			case 'r':
+				strncpy(rom_path, optarg, 256);
+				break;
+
+			case 'E':
+				extract_sprites = 1;
+				strncpy(sprites_path, optarg, 256);
+				break;
+
+			case 'M':
+				modify_sprites = 1;
+				strncpy(sprites_path, optarg, 256);
+				break;
+
+			case 'H':
+				gen_header = 1;
+				break;
+
+			case 'l':
+				strncpy(save_path, optarg, 256);
+				break;
+
+			case 's':
+				tamalib_set_exec_mode(EXEC_MODE_STEP);
+				break;
+
+			case 'b':
+				tamalib_add_bp(&g_breakpoints, strtoul(optarg, NULL, 0));
+				break;
+
+			case 'm':
+				log_levels |= LOG_MEMORY;
+				break;
+
+#if !defined(__WIN32__)
+			case 'e':
+				memory_editor_enable = 1;
+				break;
+#endif
+
+			case 'c':
+				log_levels |= LOG_CPU;
+				break;
+
+			case 'v':
+				log_levels |= LOG_MEMORY | LOG_CPU;
+				break;
+
+			case 'h':
+				usage(stdout, argc, argv);
+				exit(EXIT_SUCCESS);
+
+			default:
+				usage(stderr, argc, argv);
+				exit(EXIT_FAILURE);
+		}
+	}
+	*/
+
+	g_program = program_load(&g_program_size);
+	if (g_program == NULL) {
+		hal_log(LOG_ERROR, "FATAL: Error while loading ROM %s !", rom_path);
+		tamalib_free_bp(&g_breakpoints);
+		nocashMessage("FATAL: Error while loading ROM");
+	}
+	else{
+		nocashMessage("Load ROM OK\n");
+	}
+
+	if (gen_header || extract_sprites || modify_sprites) {
+		nocashMessage("ROM manipulation only \n");
+		// ROM manipulation only (no emulation) 
+		if (gen_header) {
+			program_to_header(g_program, g_program_size);
+		} else if (extract_sprites) {
+			program_get_data(g_program, g_program_size, sprites_path);
+		} else if (modify_sprites) {
+			program_set_data(g_program, g_program_size, sprites_path);
+			program_save(rom_path, g_program, g_program_size);
+		}
+
+		TGDSARM9Free(g_program);
+		tamalib_free_bp(&g_breakpoints);
+	}
+	else{
+		nocashMessage("ROM running normally.\n");
+	}
+
+	compute_layout();
+	
+	#ifdef WIN32
+	if (sdl_init()) {
+		hal_log(LOG_ERROR, "FATAL: Error while initializing application !");
+		TGDSARM9Free(g_program);
+		tamalib_free_bp(&g_breakpoints);
+		return -1;
+	}
+	#endif
+	
+	if (tamalib_init(g_program, NULL, 1000)) {
+		hal_log(LOG_ERROR, "FATAL: Error while initializing tamalib !");
+		nocashMessage("FATAL: Error while initializing tamalib !");
+		#ifdef WIN32
+		sdl_release();
+		#endif
+		TGDSARM9Free(g_program);
+		tamalib_free_bp(&g_breakpoints);
+	}
+	else{
+		nocashMessage("tamalib init OK !");
+	}
+    cpu_set_speed(0);
+    cycle_count = (u32*)cpu_state.tick_counter;
+
+	state_load(SAVE_PATH);
+	
+	/*
+	if (memory_editor_enable) {
+		// Logs are not compatible with the memory editor 
+		log_levels = LOG_ERROR;
+		mem_edit_configure_terminal();
+	}*/
+
+	//Tama process init end--
+	
+	enableDrawing();	// Ensure Woopsi can now draw itself
+	redraw();			// Draw initial state
+	
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void WoopsiTemplate::shutdown()   {
+	Woopsi::shutdown();
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void WoopsiTemplate::waitForAOrTouchScreenButtonMessage(MultiLineTextBox* thisLineTextBox, const WoopsiString& thisText)   {
+	thisLineTextBox->appendText(thisText);
+	scanKeys();
+	while((!(keysDown() & KEY_A)) && (!(keysDown() & KEY_TOUCH))){
+		scanKeys();
+	}
+	scanKeys();
+	while((keysDown() & KEY_A) && (keysDown() & KEY_TOUCH)){
+		scanKeys();
+	}
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void WoopsiTemplate::handleValueChangeEvent(const GadgetEventArgs& e)   {
+
+	// Did a gadget fire this event?
+	if (e.getSource() != NULL) {
+		/* 
+		//Destroyable Textbox implementation init
+		Rect rect;
+		uiScreen->getClientRect(rect);
+		_MultiLineTextBoxLogger = new MultiLineTextBox(rect.x, rect.y, 262, 170, "Loading\n...", Gadget::GADGET_DRAGGABLE, 5);
+		uiScreen->addGadget(_MultiLineTextBoxLogger);
+		
+		_MultiLineTextBoxLogger->removeText(0);
+		_MultiLineTextBoxLogger->moveCursorToPosition(0);
+		_MultiLineTextBoxLogger->appendText("File open OK: ");
+		_MultiLineTextBoxLogger->appendText(strObj);
+		_MultiLineTextBoxLogger->appendText("\n");
+		_MultiLineTextBoxLogger->appendText("Please wait calculating CRC32... \n");
+		
+		char arrBuild[256+1];
+		sprintf(arrBuild, "%s%x\n", "Invalid file: crc32 = 0x", crc32);
+		_MultiLineTextBoxLogger->appendText(WoopsiString(arrBuild));
+		
+		sprintf(arrBuild, "%s%x\n", "Expected: crc32 = 0x", 0x5F35977E);
+		_MultiLineTextBoxLogger->appendText(WoopsiString(arrBuild));
+		
+		waitForAOrTouchScreenButtonMessage(_MultiLineTextBoxLogger, "Press (A) or tap touchscreen to continue. \n");
+		
+		_MultiLineTextBoxLogger->invalidateVisibleRectCache();
+		uiScreen->eraseGadget(_MultiLineTextBoxLogger);
+		_MultiLineTextBoxLogger->destroy();	//same as delete _MultiLineTextBoxLogger;
+		//Destroyable Textbox implementation end
+		*/
+	}
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void WoopsiTemplate::handleLidClosed() {
+	// Lid has just been closed
+	_lidClosed = true;
+
+	// Run lid closed on all gadgets
+	s32 i = 0;
+	while (i < _gadgets.size()) {
+		_gadgets[i]->lidClose();
+		i++;
+	}
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void WoopsiTemplate::handleLidOpen() {
+	// Lid has just been opened
+	_lidClosed = false;
+
+	// Run lid opened on all gadgets
+	s32 i = 0;
+	while (i < _gadgets.size()) {
+		_gadgets[i]->lidOpen();
+		i++;
+	}
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void printMessage(char * msg){
+	//Destroyable Textbox implementation init
+	Rect rect;
+	WoopsiTemplateProc->uiScreen->getClientRect(rect);
+	WoopsiTemplateProc->_MultiLineTextBoxLogger = new MultiLineTextBox(rect.x, rect.y, 262, 170, "Loading\n...", Gadget::GADGET_DRAGGABLE, 5);
+	WoopsiTemplateProc->uiScreen->addGadget(WoopsiTemplateProc->_MultiLineTextBoxLogger);
+	
+	WoopsiTemplateProc->_MultiLineTextBoxLogger->removeText(0);
+	WoopsiTemplateProc->_MultiLineTextBoxLogger->moveCursorToPosition(0);
+	WoopsiTemplateProc->_MultiLineTextBoxLogger->appendText(":");
+	WoopsiTemplateProc->_MultiLineTextBoxLogger->appendText("\n");
+	
+	WoopsiTemplateProc->_MultiLineTextBoxLogger->appendText(WoopsiString(msg));
+	
+	WoopsiTemplateProc->waitForAOrTouchScreenButtonMessage(WoopsiTemplateProc->_MultiLineTextBoxLogger, "Press (A) or tap touchscreen to continue. \n");
+	
+	WoopsiTemplateProc->_MultiLineTextBoxLogger->invalidateVisibleRectCache();
+	WoopsiTemplateProc->uiScreen->eraseGadget(WoopsiTemplateProc->_MultiLineTextBoxLogger);
+	WoopsiTemplateProc->_MultiLineTextBoxLogger->destroy();	//same as delete _MultiLineTextBoxLogger;
+	//Destroyable Textbox implementation end
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void WoopsiTemplate::handleClickEvent(const GadgetEventArgs& e)   {
+	switch (e.getSource()->getRefcon()) {
+		//Tamagotchi Left Button Event
+		case 1:{
+			bankedButtons |= KEY_LEFT;
+		}	
+		break;
+		
+		//Tamagotchi Middle Button Event
+		case 2:{
+			bankedButtons |= KEY_DOWN;
+		}	
+		break;
+		
+		//Tamagotchi Right Button Event
+		case 3:{
+			bankedButtons |= KEY_RIGHT;
+		}	
+		break;
+		
+		//Tamagotchi Save & Exit Button Event
+		case 4:{
+			//Destroyable Textbox implementation init
+			Rect rect;
+			WoopsiTemplateProc->uiScreen->getClientRect(rect);
+			WoopsiTemplateProc->_MultiLineTextBoxLogger = new MultiLineTextBox(rect.x, rect.y, 262, 170, "Loading\n...", Gadget::GADGET_DRAGGABLE, 5);
+			WoopsiTemplateProc->uiScreen->addGadget(WoopsiTemplateProc->_MultiLineTextBoxLogger);
+			
+			WoopsiTemplateProc->_MultiLineTextBoxLogger->removeText(0);
+			WoopsiTemplateProc->_MultiLineTextBoxLogger->moveCursorToPosition(0);
+			WoopsiTemplateProc->_MultiLineTextBoxLogger->appendText(":");
+			WoopsiTemplateProc->_MultiLineTextBoxLogger->appendText("\n");
+			
+			WoopsiTemplateProc->_MultiLineTextBoxLogger->appendText("Are you sure you want to Save & Exit?\n(A) No\n(B) Yes");
+			bool pressedB = false;
+			while(1==1){
+				scanKeys();
+				if(keysDown() & KEY_A){
+					break;
+				}
+				if(keysDown() & KEY_B){
+					pressedB = true;
+					break;
+				}
+			}
+			WoopsiTemplateProc->_MultiLineTextBoxLogger->invalidateVisibleRectCache();
+			WoopsiTemplateProc->uiScreen->eraseGadget(WoopsiTemplateProc->_MultiLineTextBoxLogger);
+			WoopsiTemplateProc->_MultiLineTextBoxLogger->destroy();	//same as delete _MultiLineTextBoxLogger;
+			//Destroyable Textbox implementation end
+			
+			
+			if(pressedB == true){
+				state_save(SAVE_PATH);
+				printMessage("Tamagotchi has been saved correctly. It's safe to turn off unit now.");
+				shutdownNDSHardware();
+				while(1==1){
+					IRQWait(0, IRQ_VBLANK);
+				}
+			}
+		}	
+		break;
+		
+		
+	}
+}
+
+__attribute__((section(".dtcm")))
+u32 pendPlay = 0;
+
+char currentFileChosen[256+1];
+
+//Called once Woopsi events are ended: TGDS Main Loop
+__attribute__((section(".itcm")))
+void Woopsi::ApplicationMainLoop()  {
+	//Earlier.. main from Woopsi SDK.
+	
+	//Handle TGDS stuff...
+	tama_process();
+}
---
+++ arm9/source/main.cpp
@@ -0,0 +1,221 @@
+/*
+
+			Copyright (C) 2017  Coto
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
+USA
+
+*/
+
+#include "main.h"
+#include "typedefsTGDS.h"
+#include "dsregs.h"
+#include "dswnifi_lib.h"
+#include "keypadTGDS.h"
+#include "fileBrowse.h"	//generic template functions from TGDS: maintain 1 source, whose changes are globally accepted by all TGDS Projects.
+#include "biosTGDS.h"
+#include "ipcfifoTGDSUser.h"
+#include "dldi.h"
+#include "global_settings.h"
+#include "posixHandleTGDS.h"
+#include "TGDSMemoryAllocator.h"
+#include "consoleTGDS.h"
+#include "soundTGDS.h"
+#include "nds_cp15_misc.h"
+#include "fatfslayerTGDS.h"
+#include "utilsTGDS.h"
+#include "ima_adpcm.h"
+#include "tamalib.h"
+#include "mem_edit.h"
+
+// Includes
+#include "WoopsiTemplate.h"
+
+//TGDS Soundstreaming API
+int internalCodecType = SRC_NONE; //Returns current sound stream format: WAV, ADPCM or NONE
+struct fd * _FileHandleVideo = NULL; 
+struct fd * _FileHandleAudio = NULL;
+
+bool stopSoundStreamUser(){
+	return stopSoundStream(_FileHandleVideo, _FileHandleAudio, &internalCodecType);
+}
+
+void closeSoundUser(){
+	//Stubbed. Gets called when closing an audiostream of a custom audio decoder
+}
+
+//because virtual pet needs drums to start properly
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+void playTamaIntro(){
+	playSoundStream("0:/drumSample.ima", _FileHandleVideo, _FileHandleAudio);
+}
+
+static inline void menuShow(){
+	clrscr();
+	printf("     ");
+	printf("     ");
+	printf("toolchaingenericds-foobilliard: ");
+	printf("(Select): This menu. ");
+	printf("(Start): FileBrowser : (A) Play WAV/IMA-ADPCM (Intel) strm ");
+	printf("(D-PAD:UP/DOWN): Volume + / - ");
+	printf("(D-PAD:LEFT): GDB Debugging. >%d", TGDSPrintfColor_Green);
+	printf("(D-PAD:RIGHT): Demo Sound. >%d", TGDSPrintfColor_Yellow);
+	printf("(B): Stop WAV/IMA-ADPCM file. ");
+	printf("Current Volume: %d", (int)getVolume());
+	if(internalCodecType == SRC_WAVADPCM){
+		printf("ADPCM Play: >%d", TGDSPrintfColor_Red);
+	}
+	else if(internalCodecType == SRC_WAV){	
+		printf("WAVPCM Play: >%d", TGDSPrintfColor_Green);
+	}
+	else{
+		printf("Player Inactive");
+	}
+	printf("Available heap memory: %d >%d", getMaxRam(), TGDSPrintfColor_Cyan);
+}
+
+//ToolchainGenericDS-LinkedModule User implementation: Called if TGDS-LinkedModule fails to reload ARM9.bin from DLDI.
+char args[8][MAX_TGDSFILENAME_LENGTH];
+char *argvs[8];
+int TGDSProjectReturnFromLinkedModule()  {
+	return -1;
+}
+
+#if (defined(__GNUC__) && !defined(__clang__))
+__attribute__((optimize("O0")))
+#endif
+
+#if (!defined(__GNUC__) && defined(__clang__))
+__attribute__ ((optnone))
+#endif
+int main(int argc, char **argv)   {
+	
+	/*			TGDS 1.6 Standard ARM9 Init code start	*/
+	bool isTGDSCustomConsole = false;	//set default console or custom console: default console
+	GUI_init(isTGDSCustomConsole);
+	GUI_clear();
+	/*			TGDS 1.6 Standard ARM9 Init code end	*/
+	
+	/////////////////////////////////////////////////////////Reload TGDS Proj///////////////////////////////////////////////////////////
+	#if !defined(ISEMULATOR)
+	char tmpName[256];
+	char ext[256];	
+	char TGDSProj[256];
+	char curChosenBrowseFile[256];
+	strcpy(TGDSProj,"0:/");
+	strcat(TGDSProj, "ToolchainGenericDS-multiboot");
+	if(__dsimode == true){
+		strcat(TGDSProj, ".srl");
+	}
+	else{
+		strcat(TGDSProj, ".nds");
+	}
+	//Force ARM7 reload once 
+	if( 
+		(argc < 3) 
+		&& 
+		(strncmp(argv[1], TGDSProj, strlen(TGDSProj)) != 0) 	
+	){
+		REG_IME = 0;
+		MPUSet();
+		REG_IME = 1;
+		char startPath[MAX_TGDSFILENAME_LENGTH+1];
+		strcpy(startPath,"/");
+		strcpy(curChosenBrowseFile, TGDSProj);
+		
+		char thisTGDSProject[MAX_TGDSFILENAME_LENGTH+1];
+		strcpy(thisTGDSProject, "0:/");
+		strcat(thisTGDSProject, TGDSPROJECTNAME);
+		if(__dsimode == true){
+			strcat(thisTGDSProject, ".srl");
+		}
+		else{
+			strcat(thisTGDSProject, ".nds");
+		}
+		
+		//Boot .NDS file! (homebrew only)
+		strcpy(tmpName, curChosenBrowseFile);
+		separateExtension(tmpName, ext);
+		strlwr(ext);
+		
+		//pass incoming launcher's ARGV0
+		char arg0[256];
+		int newArgc = 3;
+		if (argc > 2) {
+			printf(" ---- test");
+			printf(" ---- test");
+			printf(" ---- test");
+			printf(" ---- test");
+			printf(" ---- test");
+			printf(" ---- test");
+			printf(" ---- test");
+			printf(" ---- test");
+			
+			//arg 0: original NDS caller
+			//arg 1: this NDS binary
+			//arg 2: this NDS binary's ARG0: filepath
+			strcpy(arg0, (const char *)argv[2]);
+			newArgc++;
+		}
+		//or else stub out an incoming arg0 for relaunched TGDS binary
+		else {
+			strcpy(arg0, (const char *)"0:/incomingCommand.bin");
+			newArgc++;
+		}
+		//debug end
+		
+		char thisArgv[4][MAX_TGDSFILENAME_LENGTH];
+		memset(thisArgv, 0, sizeof(thisArgv));
+		strcpy(&thisArgv[0][0], thisTGDSProject);	//Arg0:	This Binary loaded
+		strcpy(&thisArgv[1][0], curChosenBrowseFile);	//Arg1:	Chainload caller: TGDS-MB
+		strcpy(&thisArgv[2][0], thisTGDSProject);	//Arg2:	NDS Binary reloaded through ChainLoad
+		strcpy(&thisArgv[3][0], (char*)&arg0[0]);//Arg3: NDS Binary reloaded through ChainLoad's ARG0
+		addARGV(newArgc, (char*)&thisArgv);				
+		if(TGDSMultibootRunNDSPayload(curChosenBrowseFile) == false){ //should never reach here, nor even return true. Should fail it returns false
+			
+		}
+	}
+	#endif
+	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	if(__dsimode == true){
+		TWLSetTouchscreenTWLMode();
+	}
+	
+	// Create Woopsi UI
+	WoopsiTemplate WoopsiTemplateApp;
+	WoopsiTemplateProc = &WoopsiTemplateApp;
+	WoopsiTemplateApp.main(argc, argv);
+	
+	//tama process end
+	if (memory_editor_enable) {
+		mem_edit_reset_terminal();
+	}
+
+	tamalib_release();
+
+	#ifdef WIN32
+	sdl_release();
+	#endif
+	
+	TGDSARM9Free(g_program);
+	tamalib_free_bp(&g_breakpoints);
+	
+	return 0;
+}
